d1cf269ae58cc210671ce5e1cbe198e6b6cd4364
Victor Poletti <victorpoletti2009@hotmail.com>
2019-05-21 08:26:16 -0300

    Realizando upload e delete das imagens do s3

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,10 +1,11 @@
 const multer = require('multer');
+const multerS3 = require('multer-s3');
+const aws = require('aws-sdk');
 const path = require('path');
 const crypto = require('crypto');
 
-module.exports = {
-  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, '..', '..', 'tmp', 'uploads'));
     },
@@ -12,26 +13,46 @@ module.exports = {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err);
 
+        file.key = `${hash.toString('hex')}-${file.originalname}`;
+
+        cb(null, file.key);
+      });
+    },
+  }),
+  s3: multerS3({
+    s3: new aws.S3(),
+    bucket: 'upload-server-example',
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: 'public-read',
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
         const fileName = `${hash.toString('hex')}-${file.originalname}`;
 
         cb(null, fileName);
       });
-    }
+    },
   }),
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, '..', '..', 'tmp', 'uploads'),
+  storage: storageTypes[process.env.STORAGE_TYPE],
   limits: {
-    fileSize: 2 * 1024 * 1024
+    fileSize: 2 * 1024 * 1024,
   },
   fileFilter: (req, file, cb) => {
     const allowedMimes = [
       'image/jpeg',
       'image/pjpeg',
       'image/png',
-      'image/gif'
+      'image/gif',
     ];
     if (allowedMimes.includes(file.mimetype)) {
       cb(null, true);
     } else {
       cb(new Error('Invalid file type.'));
     }
-  }
+  },
 };
